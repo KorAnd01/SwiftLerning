99ca1fe4f532e6efbc3805275840a82bdd080ae1
Andrey <dronisey@mail.ru>
2025-03-07 23:07:10 +0100

    3.2.1

---
+++ Профиль — копия 3.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+
+let userName: String = "Andrey Korobko"
+let userCitizenship: String = "Rus"
+var userAge: UInt = 38
+let userHeight: Double = 193.5
+var userVeight: Float = 90.3
+var userMarrid: Bool = true
+
+print ("имя:", userName,"гражданство:", userCitizenship,"возраст:", userAge,"рост:", userHeight,"вес:", userVeight, "семейное положение",userMarrid)
